1b29c3e4ad097082a661135ad11648b6eacf24ff
arlind00 <aarlind@live.com>
2022-05-06 11:46:23 +0200

    day2 T2 2nd commit

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T2_LinkTextPractice.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T2_LinkTextPractice.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day2_locators_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class T2_LinkTextPractice {
@@ -13,10 +15,36 @@ public class T2_LinkTextPractice {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
+        driver.get("https://practice.cydeo.com");
+        //  driver.findElement(By.linkText("A/B Testing")).click();
+        WebElement abTestLink = driver.findElement(By.linkText("A/B Testing"));     // element we found we assign to a variable WebElement
+        abTestLink.click();                                                         // WebElement variable that contains element from webpage
+                                                                                    // clicks on that specific element
+
+        String expectedTitle = "A/B Testing";
+        String actualTitile = driver.getTitle();
+
+        if (actualTitile.contains(expectedTitle)){
+            System.out.println("Title Verification passed");
+        }else   {
+            System.out.println("Title Verification failed");
+        }
+
+        driver.navigate().back();
+
+        expectedTitle = "Practice";
+        actualTitile = driver.getTitle();
+
+        if (actualTitile.contains(expectedTitle)){
+            System.out.println("Title Verification passed");
+        }else   {
+            System.out.println("Title Verification failed");
+        }
+
+
 
     }
 }
-
 
 /*
 TC #2: Back and forth navigation
