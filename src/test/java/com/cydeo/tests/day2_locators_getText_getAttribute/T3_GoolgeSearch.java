54c3e9d55412af24bb3f7f949de76e1caf342898
arlind00 <aarlind@live.com>
2022-05-06 11:58:15 +0200

    day2 T3 2nd commit

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T3_GoolgeSearch.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T3_GoolgeSearch.java
@@ -1,7 +1,10 @@
 package com.cydeo.tests.day2_locators_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class T3_GoolgeSearch {
@@ -15,6 +18,20 @@ public class T3_GoolgeSearch {
 
 
         driver.get("https://www.google.com");
+        WebElement googleSearch = driver.findElement(By.name("q"));
+        googleSearch.sendKeys("apple" + Keys.ENTER);                 // sends String from IntelliJ to webpage
+                                                                                   // then clicks ENTER
+
+
 
     }
 }
+/*
+TC#3: Google search
+        1- Open a chrome browser
+        2- Go to: https://google.com
+        3- Write “apple” in search box
+        4- Click google search button
+        5- Verify title:
+        Expected: Title should start with “apple” word
+ */
