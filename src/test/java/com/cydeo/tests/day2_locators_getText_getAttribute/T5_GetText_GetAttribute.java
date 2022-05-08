7125f105af1487b89d74ac09e9e9e17ae3d7557c
arlind00 <aarlind@live.com>
2022-05-08 06:26:54 +0200

    day2 T5 2nd commit

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T5_GetText_GetAttribute.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T5_GetText_GetAttribute.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day2_locators_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class T5_GetText_GetAttribute {
@@ -12,11 +14,25 @@ public class T5_GetText_GetAttribute {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
+        driver.get("https://practice.cydeo.com/registration_form");
+
+        WebElement headerText =driver.findElement(By.tagName("h2"));
+
+        String actualHeaderText = headerText.getText();
+        String expectedHeaderText = "Registration form";
+
+        if (actualHeaderText.startsWith(expectedHeaderText)){
+            System.out.println("Title Verification passed");
+        }else   {
+            System.out.println("Title Verification failed");
+        }
+
 
     }
-
-
 }
+
+
+
 /*
 TC #5: getText() and getAttribute() method practice
 1- Open a chrome browser
