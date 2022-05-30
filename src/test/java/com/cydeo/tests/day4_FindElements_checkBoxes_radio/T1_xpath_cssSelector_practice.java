e50c3dab19a0b828855b53a6807b3b6f5109d392
arlind00 <aarlind@live.com>
2022-05-30 23:27:29 +0200

    day4 T1

--- src/test/java/com/cydeo/tests/day4_FindElements_checkBoxes_radio/T1_xpath_cssSelector_practice.java
+++ src/test/java/com/cydeo/tests/day4_FindElements_checkBoxes_radio/T1_xpath_cssSelector_practice.java
@@ -1,4 +1,63 @@
 package com.cydeo.tests.day4_FindElements_checkBoxes_radio;
 
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class T1_xpath_cssSelector_practice {
+
+    public static void main(String[] args) {
+
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.get("https://practice.cydeo.com/forgot_password");
+
+        WebElement homeLink_ex1= driver.findElement(By.cssSelector("a[class='nav-link']"));   // syntax 1
+        WebElement homeLink_ex2= driver.findElement(By.cssSelector("a.nav-link"));            // syntax 2
+        WebElement homeLink_ex3= driver.findElement(By.cssSelector("a[href='/']"));           // syntax 3
+
+
+        WebElement header_ex1 = driver.findElement(By.cssSelector("div.example > h2"));             // syntax 1
+        WebElement header_ex2 = driver.findElement(By.xpath("//h2[text()='Forgot Password']"));     // syntax 2
+
+
+        WebElement emailLabel = driver.findElement(By.xpath("//label[@for='email']"));
+
+
+        WebElement inputBox_ex1 = driver.findElement(By.xpath("//input[@name='email']"));
+        WebElement inputBox_ex2 = driver.findElement(By.xpath("//input[contains(@pattern,'a-z')]"));
+
+
+        WebElement retrievePasswordBtn = driver.findElement(By.xpath("//button[@id='form_submit']"));
+
+
+        WebElement poweredByCydeoText = driver.findElement(By.xpath("//div[@style='text-align: center;']"));
+
+
+        System.out.println("homeLink_ex1.isDisplayed() = " + homeLink_ex1.isDisplayed());
+        System.out.println("header_ex1.isDisplayed() = " + header_ex1.isDisplayed());
+        System.out.println("emailLabel.isDisplayed() = " + emailLabel.isDisplayed());
+        System.out.println("inputBox_ex1.isDisplayed() = " + inputBox_ex1.isDisplayed());
+        System.out.println("retrievePasswordBtn.isDisplayed() = " + retrievePasswordBtn.isDisplayed());
+        System.out.println("poweredByCydeoText.isDisplayed() = " + poweredByCydeoText.isDisplayed());
+
+    }
 }
+
+
+/*
+TC #1: XPATH and cssSelector Practices
+1. Open Chrome browser
+2. Go to https://practice.cydeo.com/forgot_password
+3. Locate all the WebElements on the page using XPATH and/or CSS
+locator only (total of 6)
+a. “Home” link
+b. “Forgot password” header
+c. “E-mail” text
+d. E-mail input box
+e. “Retrieve password” button
+f. “Powered by Cydeo text
+4. Verify all web elements are displayed.
+ */
